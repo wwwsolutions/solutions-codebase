5ad37a0cf95879e772469abf06ba7b09250663dc
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2020-12-06 18:24:06 +0100

    added todomongoapp web app + todomongoapi express app

---
+++ apps/todomongoapi/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'todomongoapi',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/todomongoapi',
+};
