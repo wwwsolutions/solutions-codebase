bb2b145f1928038d9506ff3efa9aafc8d49855d9
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2020-12-08 18:53:13 +0100

    created new route tours POST

--- apps/natoursapi/src/main.ts
+++ apps/natoursapi/src/main.ts
@@ -1,14 +1,26 @@
 import * as express from 'express';
 
+// FAKE DATA
+import { tours } from './dev-data/data/tours-simple';
+
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.status(200).send({ message: 'Welcome to natoursapi! GET' });
+app.get('/api/v1/tours', (req, res) => {
+  // ENVELOPE DATA
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours },
+  });
 });
 
-app.post('/api', (req, res) => {
-  res.status(200).send({ message: 'Welcome to natoursapi! POST' });
-});
+// app.get('/api', (req, res) => {
+//   res.status(200).send({ message: 'Welcome to natoursapi! GET' });
+// });
+
+// app.post('/api', (req, res) => {
+//   res.status(200).send({ message: 'Welcome to natoursapi! POST' });
+// });
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
