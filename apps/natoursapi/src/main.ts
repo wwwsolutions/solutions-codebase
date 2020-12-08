b17d270ff02438ad9749a431d11e8a41dc75929e
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2020-12-08 22:04:51 +0100

    refactored route handlers > extracted to separate functions

--- apps/natoursapi/src/main.ts
+++ apps/natoursapi/src/main.ts
@@ -3,22 +3,15 @@ import * as express from 'express';
 // FAKE DATA
 import { tours } from './dev-data/data/tours-simple';
 
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// Get all tours
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: { tours },
   });
-});
+};
 
-// Get one tour
-app.get('/api/v1/tours/:id', (req, res) => {
+const getOneTour = (req, res) => {
   const { id } = req.params;
   const tour = tours.find((el) => el.id === parseInt(id));
 
@@ -33,11 +26,22 @@ app.get('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: { tour },
   });
-});
+};
 
-// DELETE
-// app.patch('', (req, res) =>)
-app.delete('/api/v1/tours/:id', (req, res) => {
+const createTour = (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  res.status(201).json({
+    status: 'success',
+    data: { tour: newTour },
+  });
+};
+
+const deleteTour = (req, res) => {
   const { id } = req.params;
 
   if (parseInt(id) > tours.length) {
@@ -51,11 +55,9 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: null,
   });
-});
+};
 
-// PUT, PATCH
-// app.patch('', (req, res) =>)
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
   const { id } = req.params;
 
   if (parseInt(id) > tours.length) {
@@ -71,29 +73,30 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tour: 'Updated tour here',
     },
   });
-});
+};
+
+//''''''''''''''''''''''''''
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Get all tours
+app.get('/api/v1/tours', getAllTours);
+
+// Get one tour
+app.get('/api/v1/tours/:id', getOneTour);
+
+// DELETE
+// app.patch('', (req, res) =>)
+app.delete('/api/v1/tours/:id', deleteTour);
+
+// PUT, PATCH
+app.patch('/api/v1/tours/:id', updateTour);
 
 // Create new tour
-app.post('/api/v1/tours', (req, res) => {
-  // console.log(req.body);
-
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-
-  res.status(201).json({
-    status: 'success',
-    data: { tour: newTour },
-  });
-});
-
-// app.get('/api', (req, res) => {
-//   res.status(200).send({ message: 'Welcome to natoursapi! GET' });
-// });
-
-// app.post('/api', (req, res) => {
-//   res.status(200).send({ message: 'Welcome to natoursapi! POST' });
-// });
+app.post('/api/v1/tours', createTour);
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
