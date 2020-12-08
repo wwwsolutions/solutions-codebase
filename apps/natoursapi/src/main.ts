d2d9aa2e3ef3448a0a1c0df3380fe026d03ad86b
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2020-12-08 16:02:05 +0100

    generated server with basic routes

--- apps/natoursapi/src/main.ts
+++ apps/natoursapi/src/main.ts
@@ -1,14 +1,13 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import * as express from 'express';
 
 const app = express();
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to natoursapi!' });
+  res.status(200).send({ message: 'Welcome to natoursapi! GET' });
+});
+
+app.post('/api', (req, res) => {
+  res.status(200).send({ message: 'Welcome to natoursapi! POST' });
 });
 
 const port = process.env.port || 3333;
