2d080f293987e838349f7ca327a5405d455bb1b8
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2020-12-09 12:22:45 +0100

    implemented user-controller

--- libs/natoursapi/routes/src/lib/user-routes.ts
+++ libs/natoursapi/routes/src/lib/user-routes.ts
@@ -1,39 +1,12 @@
 import { Router, Request, Response } from 'express';
 
-const getUsers = (req: Request, res: Response) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined.',
-  });
-};
-
-const getUser = (req: Request, res: Response) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined.',
-  });
-};
-
-const createUser = (req: Request, res: Response) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined.',
-  });
-};
-
-const deleteUser = (req: Request, res: Response) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined.',
-  });
-};
-
-const updateUser = (req: Request, res: Response) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined.',
-  });
-};
+import {
+  getUsers,
+  getUser,
+  createUser,
+  deleteUser,
+  updateUser,
+} from '@codebase/natoursapi/controllers';
 
 export const userRouter = Router();
 userRouter.route('/').get(getUsers).post(createUser);
