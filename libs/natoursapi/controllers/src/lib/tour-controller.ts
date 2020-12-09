d59bf386283261e1a5f35144eea0051751c6b091
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2020-12-09 12:20:40 +0100

    implemented tour-controller

--- libs/natoursapi/controllers/src/lib/tour-controller.ts
+++ libs/natoursapi/controllers/src/lib/tour-controller.ts
@@ -1,3 +1,75 @@
-export function tourController(): string {
-  return 'tour-controller';
-}
+import { Request, Response } from 'express';
+import { toursSimple as tours } from '@codebase/natoursapi/fake-data';
+
+export const getTours = (req: Request, res: Response) => {
+  // console.log(req.requestTime);
+
+  res.status(200).json({
+    status: 'success',
+    // requestedAt: req.requestTime,
+    results: tours.length,
+    data: { tours },
+  });
+};
+
+export const getTour = (req: Request, res: Response) => {
+  const { id } = req.params;
+  const tour = tours.find((el) => el.id === parseInt(id));
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+};
+
+export const createTour = (req: Request, res: Response) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  res.status(201).json({
+    status: 'success',
+    data: { tour: newTour },
+  });
+};
+
+export const deleteTour = (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  if (parseInt(id) > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+export const updateTour = (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  if (parseInt(id) > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: 'Updated tour here',
+    },
+  });
+};
