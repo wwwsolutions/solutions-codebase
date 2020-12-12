3b7e4c3527fdf005b4e4703efe4b783baf03ebe5
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2020-12-12 11:30:34 +0100

    added querystring advanced filtering

--- libs/natoursapi/controllers/src/lib/tour-controller.ts
+++ libs/natoursapi/controllers/src/lib/tour-controller.ts
@@ -18,16 +18,25 @@ export const createTour = async (req: Request, res: Response) => {
 };
 
 export const getTours = async (req: Request, res: Response): Promise<void> => {
+  console.log(req.query);
   try {
     // PREPARE QUERYSTRING
-    // TODO: candidate for ramda implementation
 
     // BUILD QUERY
+
+    // FILTERING
+
+    // TODO: candidate for ramda implementation
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    // ADVANCED FILTERING
+    // TODO: candidate for ramda implementation
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    const query = Tour.find(JSON.parse(queryStr));
 
     // EXECUTE QUERY
     const tours = await query;
