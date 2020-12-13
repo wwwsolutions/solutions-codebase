5d801c8b6c004d1a1ebab03f67843a2df576e34c
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2020-12-13 09:40:50 +0100

    added getMonthlyPlan aggregation

--- libs/natoursapi/routes/src/lib/tour-routes.ts
+++ libs/natoursapi/routes/src/lib/tour-routes.ts
@@ -6,7 +6,9 @@ import {
   deleteTour,
   updateTour,
   aliasTopTours,
+  // AGGREGATIONS
   getTourStats,
+  getMonthlyPlan,
 } from '@codebase/natoursapi/controllers';
 
 export const tourRouter = Router();
@@ -15,6 +17,9 @@ export const tourRouter = Router();
 // tourRouter.param('id', checkId);
 
 tourRouter.route('/tour-stats').get(getTourStats);
+tourRouter.route('/monthly-plan/:year').get(getMonthlyPlan);
+
 tourRouter.route('/top-5-cheap').get(aliasTopTours, getTours);
+
 tourRouter.route('/').get(getTours).post(createTour);
 tourRouter.route('/:id').get(getTour).delete(deleteTour).patch(updateTour);
