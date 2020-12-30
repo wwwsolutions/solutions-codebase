efd72ab3b49309302cb07770deffe65f0c46cfe0
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2020-12-30 16:29:22 +0100

    added protect middleware

--- libs/natoursapi/routes/src/lib/tour-routes.ts
+++ libs/natoursapi/routes/src/lib/tour-routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import {
+  protect,
   getAllTours,
   getTour,
   createTour,
@@ -16,7 +17,8 @@ const router = Router();
 router.route('/tour-stats').get(getTourStats);
 router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
-router.route('/').get(getAllTours).post(createTour);
+
+router.route('/').get(protect, getAllTours).post(createTour);
 router.route('/:id').get(getTour).delete(deleteTour).patch(updateTour);
 
 export { router as tourRouter };
