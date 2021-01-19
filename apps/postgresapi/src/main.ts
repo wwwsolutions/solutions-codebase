005fc27b845fd3c015c8b46af5563e77f380c773
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2021-01-19 14:20:50 +0100

    edited createLesson query

--- apps/postgresapi/src/main.ts
+++ apps/postgresapi/src/main.ts
@@ -1,12 +1,14 @@
-import { environment } from '@codebase/shared/environments';
 import app from './app/app';
+import { environment } from '@codebase/shared/environments';
 
 app.get('/api', (req, res) => {
   res.send({ message: `Welcome to Postgresapi!` });
 });
 
 const port = environment.apiPort || 5000;
+
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
+
 server.on('error', console.error);
--- libs/postgresapi/queries/src/lib/create-lesson.query.ts
+++ libs/postgresapi/queries/src/lib/create-lesson.query.ts
@@ -3,5 +3,6 @@
 import { LessonModel } from '@codebase/postgresapi/models';
 
 export function createLesson(props: any) {
+  console.log('props=== ', props);
   return LessonModel.create(props);
 }
