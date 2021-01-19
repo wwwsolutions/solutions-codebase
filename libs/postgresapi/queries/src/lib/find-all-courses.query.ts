fdb6232d8ade6644e9141b604b9e03531ce567e0
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2021-01-19 22:46:05 +0100

    addded course-datail interface

--- libs/postgresapi/queries/src/lib/find-all-courses.query.ts
+++ libs/postgresapi/queries/src/lib/find-all-courses.query.ts
@@ -1,13 +1,14 @@
-import {
-  CourseModel,
-  createCourseSummaries,
-} from '@codebase/postgresapi/models';
-import { CourseSummary } from '@codebase/shared/data-access-models';
-
 // https://www.zleptnig.com/blog/the-power-of-bluebirds-promise-map
 // https://itnext.io/bluebirds-bad-practice-docs-f9cc94b1af9
 // https://codesandbox.io/s/zrx80449jx?file=/lib/models/Actor.ts:351-356
-import Bluebird from 'bluebird';
+
+import * as Bluebird from 'bluebird';
+
+import { CourseModel } from '@codebase/postgresapi/models';
+import {
+  CourseSummary,
+  createCourseSummaries,
+} from '@codebase/shared/data-access-models';
 
 export function findAllCourses(): Bluebird<CourseSummary[]> {
   return CourseModel.findAll({ order: [`seqNo`] }).then(
