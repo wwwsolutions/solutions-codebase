1ce4d40c27926f389c6f9abd70aa874ccda30c7a
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2021-01-20 18:29:53 +0100

    generated postgres-ng-app lib

---
+++ libs/postgres-ng-app/ui/src/lib/postgres-ng-app-ui.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CourseDetailComponent } from './course-detail/course-detail.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [CourseDetailComponent],
+})
+export class PostgresNgAppUiModule {}
