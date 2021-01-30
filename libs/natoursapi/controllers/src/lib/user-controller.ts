b61c75799cae2317ba89a83a699b68db0c45812e
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2021-01-30 09:50:30 +0100

    refactored controllers

--- libs/natoursapi/controllers/src/lib/user-controller.ts
+++ libs/natoursapi/controllers/src/lib/user-controller.ts
@@ -3,12 +3,13 @@ import { NextFunction, Request, Response } from 'express';
 import { catchAsync } from '@codebase/natoursapi/utils';
 import { User } from '@codebase/natoursapi/models';
 
+// @desc    Get all users
+// @route   POST /api/users
+// @access  Public
 export const getAllUsersController = catchAsync(
-  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    // EXECUTE QUERY
+  async (req: Request, res: Response): Promise<void> => {
     const users = await User.find();
 
-    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: users.length,
@@ -17,11 +18,7 @@ export const getAllUsersController = catchAsync(
   }
 );
 
-export const getUserController = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const getUserController = (req: Request, res: Response) => {
   res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined.',
--- libs/natoursapi/models/src/lib/user-model.ts
+++ libs/natoursapi/models/src/lib/user-model.ts
@@ -5,15 +5,15 @@ import * as bcrypt from 'bcryptjs';
 
 // INTERFACES
 //--------------------------------------------------------------------------------------------------
-interface CorrectPassword {
-  correctPassword(
+interface hasCorrectPassword {
+  hasCorrectPassword(
     candidatePassword: string,
     userPassword: string
   ): Promise<boolean>;
 }
 
-export interface UserDocument extends Document, CorrectPassword {
-  correctPassword(password: string, userPassword: string): Promise<boolean>;
+export interface UserDocument extends Document, hasCorrectPassword {
+  hasCorrectPassword(password: string, userPassword: string): Promise<boolean>;
   changedPasswordAfter(iat: Date);
   name: string;
   email: string;
@@ -53,14 +53,15 @@ export const userSchema: Schema = new Schema(
       type: String,
       required: [true, 'Please provide a password'],
       minlength: 8,
-      select: false,
+      select: false, // { select : false } means the field will not be queried from the database at all.
     },
     passwordConfirm: <unknown>{
       type: String,
       required: [true, `Please confirm your password.`],
       validate: {
         validator: function (el) {
-          // it only works on 'create()'  and 'save()'
+          // Only works on CREATE and SAVE
+          // When updating user, implement >>> User.save() method!!!
           return el === this.password;
         },
         message: 'Passwords are not the same!',
@@ -78,25 +79,18 @@ export const userSchema: Schema = new Schema(
 
 // HOOKS
 //--------------------------------------------------------------------------------------------------
-userSchema.pre<UserDocument>(
-  'save',
-  async function name(next: NextFunction): Promise<void> {
-    // IF PASSWORD HAS NOT BEEN MODIFIED --> EXIT
-    if (!this.isModified('password')) return next();
-
-    // GENERATE HASH
-    this.password = await bcrypt.hash(this.password, 12);
-
-    // DELETE NOW UNNECESSARY FIELD
-    this.passwordConfirm = undefined;
-
-    next();
-  }
-);
+userSchema.pre<UserDocument>('save', async function name(
+  next: NextFunction
+): Promise<void> {
+  if (!this.isModified('password')) return next(); // EXIT IF PASSWORD HAS NOT BEEN MODIFIED
+  this.password = await bcrypt.hash(this.password, 12); // HASH PASSWORD
+  this.passwordConfirm = undefined; // REMOVE REDUNDANT FIELD
+  next();
+});
 
 // METHODS
 //--------------------------------------------------------------------------------------------------
-userSchema.methods.correctPassword = async function (
+userSchema.methods.hasCorrectPassword = async function (
   candidatePassword: string,
   userPassword: string
 ): Promise<boolean> {
@@ -111,8 +105,7 @@ userSchema.methods.changedPasswordAfter = function (
     return +JwtTimestamp < changedTimestamp;
   }
 
-  // FALSE MEANS NOT CHANGED
-  return false;
+  return false; // NOT CHANGED
 };
 
 // MODEL
